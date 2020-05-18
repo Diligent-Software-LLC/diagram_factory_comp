ad975cc339d45572e919a84d1114e92d2b36fdf9
Bradley J. Tannor <bradleytannor@gmail.com>
2020-05-18 11:58:03 -0700

    Development Complete
    'require'd the Interface, Implementation, and node_diagram.

--- lib/diagram_factory_comp.rb
+++ lib/diagram_factory_comp.rb
@@ -2,10 +2,12 @@
 # under the GNU General Public License, Version 3. Refer LICENSE.txt.
 
 require_relative "diagram_factory_comp/version"
+require 'diagram_factory_int'
+require 'diagram_factory_impl'
+require 'node_diagram'
 
 # DiagramFactoryComp.
 # @class_description
 #   A DiagramFactory composition. Composites the Interface and Implementation.
 class DiagramFactoryComp
 end
-
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "diagram_factory_comp"
 
+require_relative "../lib/diagram_factory_comp"
 require "minitest/autorun"
